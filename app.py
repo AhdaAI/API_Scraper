c910de9fb74f85f99f2767879032b4c99d4d00c3
Ahda Akmalul Ilmi <ahda043@gmail.com>
2025-06-10 19:15:32 +0700

    Adding steam endpoint

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import os
 from flask import Flask
 from scraper import EpicStore
+from steam import special_offer
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
 
+
 @app.route('/')
 def homepage():
-    return "Welcome to the Epic Store Free Games API!"
+    return "No, not here, you are wrong"
+
 
 @app.route('/epic/free_games')
 def epic_free_games():
@@ -17,6 +20,20 @@ def epic_free_games():
         return data
     else:
         return {"error": "Failed to fetch free games"}, 500
+
+
+@app.route('/steam/special_offer')
+def steam_special_offer():
+    data = {
+        "code": 200,
+        "message": "",
+        "data": []
+    }
+
+    data["message"] = "Steam Special Offer"
+    data["data"] = special_offer()
+
+    return data
 
 
 if __name__ == '__main__':
